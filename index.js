8b06617ecdb9c8008078e0f27a76bb24ebdbd2b2
AlphansoTech <chaturj@alphansotech.com>
2025-01-27 12:31:17 +0530

    author,tags api implementation

--- index.js
+++ index.js
@@ -10,9 +10,27 @@ app.use(cors());
 
 const quotesFilePath = path.join(__dirname, 'quotes.json');
 
+const authorFilePath = path.join(__dirname, 'author.json');
+
+const tagsFilePath = path.join(__dirname, 'tags.json');
+
 // Create an empty quotes array if the file doesn't exist
 fs.access(quotesFilePath)
   .catch(() => fs.writeFile(quotesFilePath, '[]'))
+  .catch((err) => {
+    console.error(err);
+    process.exit(1);
+  });
+
+  fs.access(authorFilePath)
+  .catch(() => fs.writeFile(authorFilePath, '[]'))
+  .catch((err) => {
+    console.error(err);
+    process.exit(1);
+  });
+
+  fs.access(tagsFilePath)
+  .catch(() => fs.writeFile(tagsFilePath, '[]'))
   .catch((err) => {
     console.error(err);
     process.exit(1);
@@ -53,6 +71,56 @@ app.get('/quote', async (req, res) => {
     });
     // Return the quote as a JSON response
     //res.json(quotes);
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: 'Unable to fetch a quote.' });
+  }
+});
+
+app.get('/author', async (req, res) => {
+  try {
+    // Read the quotes from the JSON file
+    const data = await fs.readFile(authorFilePath, 'utf8');
+    const quotes = JSON.parse(data);
+
+    const quotesArray = quotes['results']
+    // Select a random quote
+    //const randomQuote = quotes[Math.floor(Math.random() * quotes.length)];
+    const page = parseInt(req.query.page, 10) || 1;
+   const limit = parseInt(req.query.limit, 10) || 10;
+  
+    // Calculate start and end indexes
+    const startIndex = (page - 1) * limit;
+    const endIndex = page * limit;
+  
+    // Paginated items
+    const paginatedItems = quotesArray.slice(startIndex, endIndex);
+  
+    // Total pages
+    const totalPages = Math.ceil(quotesArray.length / limit);
+  
+    res.json({
+      page,
+      limit,
+      totalItems: quotesArray.length,
+      totalPages,
+      items: paginatedItems,
+    });
+    // Return the quote as a JSON response
+    //res.json(quotes);
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: 'Unable to fetch a quote.' });
+  }
+});
+
+app.get('/tags', async (req, res) => {
+  try {
+    // Read the quotes from the JSON file
+    const data = await fs.readFile(tagsFilePath, 'utf8');
+    const tags = JSON.parse(data);
+    // Return the quote as a JSON response
+    res.json(tags);
   } catch (err) {
     console.error(err);
     res.status(500).json({ error: 'Unable to fetch a quote.' });
